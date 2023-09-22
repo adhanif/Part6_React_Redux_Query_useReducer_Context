58a5a4b588c80bdcc2e4393467412c007765c765
ad.hanif <ad.hanif@outlook.com>
2023-09-22 15:33:52 +0200

    6.3: anecdotes, step1 completed

--- anecdotes-redux/src/main.jsx
+++ anecdotes-redux/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import reducer from "../src/reducer/anecdoteReducer.js";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+const store = createStore(reducer);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
