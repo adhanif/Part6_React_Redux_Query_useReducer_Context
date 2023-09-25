08c828650186d374d2064b5d45ff8a312cd142c5
ad.hanif <ad.hanif@outlook.com>
2023-09-25 16:34:44 +0200

    6.12 Better anecdotes, step10 completed

--- anecdotes-redux/src/App.jsx
+++ anecdotes-redux/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import FilterAnecdotes from "./components/FilterAnecdotes";
+import Notification from "./components/Notification";
 
 function App() {
   return (
     <div>
       <FilterAnecdotes />
+      <Notification />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- anecdotes-redux/src/store/store.js
+++ anecdotes-redux/src/store/store.js
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import anecdoteReducer from "../reducer/anecdoteReducer";
 import FilterAnecdotesReducer from "../reducer/FilterAnecdotesReducer";
 
+import NotificationReducer from "../reducer/NotificationReducer";
+
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
     filter: FilterAnecdotesReducer,
+    notification: NotificationReducer,
   },
 });
 
