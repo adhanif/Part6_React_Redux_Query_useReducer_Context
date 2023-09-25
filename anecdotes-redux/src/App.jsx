b9b495df37f2585a829a1565b7ccba83b300b907
ad.hanif <ad.hanif@outlook.com>
2023-09-25 15:48:50 +0200

    6.10 Better anecdotes, step8 completed

--- anecdotes-redux/src/App.jsx
+++ anecdotes-redux/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <FilterAnecdotes />
-      
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- anecdotes-redux/src/main.jsx
+++ anecdotes-redux/src/main.jsx
@@ -2,17 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import anecdoteReducer from "../src/reducer/anecdoteReducer.js";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
-import Filtereducer from "../src/reducer/FilterAnecdotesReducer.js";
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: Filtereducer,
-});
-
-const store = createStore(reducer);
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
