a9504e8f897bb9612db8f4feb4158c7ffc56ef38
ad.hanif <ad.hanif@outlook.com>
2023-09-30 18:54:40 +0200

    6.19 Anecdotes and the backend, step6 completed

--- anecdotes-redux/src/components/AnecdoteForm.jsx
+++ anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { createNewAnecdote } from "../reducer/anecdoteReducer";
 
-import {
-  setNotification,
-  removeNotification,
-} from "../reducer/NotificationReducer";
+import { Notification } from "../reducer/NotificationReducer";
 
 export default function AnecdoteForm() {
   const dispatch = useDispatch();
@@ -14,11 +11,7 @@ export default function AnecdoteForm() {
     e.preventDefault();
     const content = e.target.anecdote.value;
     dispatch(createNewAnecdote(content));
-
-    dispatch(setNotification(`You created "${content}"`));
-    setTimeout(() => {
-      dispatch(removeNotification());
-    }, 2000);
+    dispatch(Notification(`You created "${content}"`));
     e.target.anecdote.value = "";
   };
 
--- anecdotes-redux/src/reducer/NotificationReducer.js
+++ anecdotes-redux/src/reducer/NotificationReducer.js
@@ -6,11 +6,22 @@ const notificationSlice = createSlice({
   name: "notification",
   initialState,
   reducers: {
-    setNotification: (state, action) => action.payload,
+    setNotification(state, action) {
+      return action.payload;
+    },
     removeNotification: (state) => null,
   },
 });
 
 export const { setNotification, removeNotification } =
   notificationSlice.actions;
+
+export const Notification = (content) => {
+  return async (dispatch) => {
+    dispatch(setNotification(content));
+    setTimeout(() => {
+      dispatch(removeNotification());
+    }, 2000);
+  };
+};
 export default notificationSlice.reducer;
