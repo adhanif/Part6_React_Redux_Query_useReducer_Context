fe811583f5e6eeb9987f1d297f84150a9fedaafb
ad.hanif <ad.hanif@outlook.com>
2023-10-02 13:32:36 +0200

    Exercise 6.22 completed

--- anecdotes-react-query/src/components/AnecdoteList.jsx
+++ anecdotes-react-query/src/components/AnecdoteList.jsx
@@ -1,29 +1,39 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getAnecdotes } from "../requests.js";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "../requests.js";
 
 export default function AnecdoteList() {
+  const queryClient = useQueryClient();
+
+  const updatedAnecdotsMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryClient: ["anecdotes"],
+      });
+    },
+  });
+
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
     retry: 1,
   });
 
   const { status } = result;
-
-  // console.log(JSON.parse(JSON.stringify(result)));
-
   if (status === "loading") {
     return <div>loading data...</div>;
   }
-
   if (status === "error") {
     return (
       <span>anecdote service not available due to problems in server</span>
     );
   }
-
   const anecdotes = result.data;
+
+  const handleVotes = (anecdote) => {
+    updatedAnecdotsMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+  };
 
   return (
     <div>
