899d598931bc7e4ca7d6f2fd7581fcafe0a21b39
ad.hanif <ad.hanif@outlook.com>
2023-10-03 15:23:11 +0200

    Exercise 6.24 completed

--- anecdotes-react-query/src/App.jsx
+++ anecdotes-react-query/src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
-import FilterAnecdotes from "./components/FilterAnecdotes";
 import Notification from "./components/Notification";
 
 function App() {
   return (
     <div>
-      {/* <FilterAnecdotes /> */}
-      <h1 >Anecdotes</h1>
+      <h1>Anecdotes</h1>
       <Notification />
       <AnecdoteList />
       <AnecdoteForm />
